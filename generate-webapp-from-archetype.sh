9528fa7edad4df7507f90026a29df573bc4fd99e
Matthew McCullough <matthewm@ambientideas.com>
2010-03-02 09:29:32 -0700

    Adding script to generate webapp from archetype

---
+++ generate-webapp-from-archetype.sh
@@ -0,0 +1,7 @@
+mvn org.apache.maven.plugins:maven-archetype-plugin:2.0-alpha-4:generate\
+ -DarchetypeGroupId=org.mulesoft.archetypes\
+ -DarchetypeArtifactId=tomcat-webapp-archetype\
+ -DarchetypeVersion=1.0-SNAPSHOT\
+ -DgroupId=com.mysamplecompany\
+ -DartifactId=my-sample-webapp\
+ -Dversion=1.0-SNAPSHOT
