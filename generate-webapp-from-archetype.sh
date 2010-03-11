15607f977bab45812532b22b18a2e76a8fab9755
Matthew McCullough <matthewm@ambientideas.com>
2010-03-11 14:51:48 -0700

    Updating to instantiate verion 1.0.4 of the archetype

--- generate-webapp-from-archetype.sh
+++ generate-webapp-from-archetype.sh
@@ -3,7 +3,7 @@ cd temp
 mvn org.apache.maven.plugins:maven-archetype-plugin:2.0-alpha-4:generate\
  -DarchetypeGroupId=org.mulesoft.archetypes\
  -DarchetypeArtifactId=tomcat-webapp-archetype\
- -DarchetypeVersion=1.0-SNAPSHOT\
+ -DarchetypeVersion=1.0.4-SNAPSHOT\
  -DgroupId=com.mysamplecompany\
  -DartifactId=temp-sample-webapp\
  -Dversion=1.0-SNAPSHOT
