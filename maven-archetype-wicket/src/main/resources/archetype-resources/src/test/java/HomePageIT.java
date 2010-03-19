eb59953f6e17aad7ff1db8b73563591bfa5e9e01
Matthew McCullough <matthewm@ambientideas.com>
2010-03-19 22:12:14 +0100

    Get Integration tests working for wicket example

--- maven-archetype-wicket/src/main/resources/archetype-resources/src/test/java/HomePageIT.java
+++ maven-archetype-wicket/src/main/resources/archetype-resources/src/test/java/HomePageIT.java
@@ -2,17 +2,19 @@ package ${package};
 
 import net.sourceforge.jwebunit.junit.WebTestCase;
 
-public class HomeJSPIT extends WebTestCase {
+public class HomePageIT extends WebTestCase {
 	
     public void setUp() throws Exception {
         super.setUp();
         setBaseUrl("http://localhost:8080/");
     }
 
-    public void testHomeJSPTitle() {
+    public void testHomePageTitle() {
         beginAt("/$artifactId");
+        assertTitleEquals("Wicket Quickstart Archetype Homepage");
+        // assertLabelPresent("message");
+        assertTextPresent("If you see this message wicket is properly configured and running");
         // clickLink("login");
-        assertTitleEquals("A sample JSP");
         // setTextField("username", "test");
         // setTextField("password", "test123");
         // submit();
