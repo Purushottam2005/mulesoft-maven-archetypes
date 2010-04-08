fe2dbab2f245eb22387c02e62f823cc5c07817e5
Matthew McCullough <matthewm@ambientideas.com>
2010-04-07 21:39:56 -0600

    Updating script to use version 1.0.6

--- generate-webapp-from-archetype.sh
+++ generate-webapp-from-archetype.sh
@@ -6,10 +6,10 @@ pushd temp
 mvn org.apache.maven.plugins:maven-archetype-plugin:2.0-alpha-4:generate\
  -DarchetypeGroupId=org.mule.tools.maven2.archetypes\
  -DarchetypeArtifactId=tomcat-webapp-archetype\
- -DarchetypeVersion=1.0.5-SNAPSHOT\
+ -DarchetypeVersion=1.0.6\
  -DgroupId=com.mysamplecompany\
  -DartifactId=temp-sample-jsp-webapp\
- -Dversion=1.0-SNAPSHOT
+ -Dversion=0.0.1-SNAPSHOT
 
 pushd temp-sample-jsp-webapp
 mvn verify
@@ -19,10 +19,10 @@ popd
 mvn org.apache.maven.plugins:maven-archetype-plugin:2.0-alpha-4:generate\
  -DarchetypeGroupId=org.mule.tools.maven2.archetypes\
  -DarchetypeArtifactId=tomcat-wicket-archetype\
- -DarchetypeVersion=1.0.5-SNAPSHOT\
+ -DarchetypeVersion=1.0.6\
  -DgroupId=com.mysamplecompany\
  -DartifactId=temp-sample-wicket-webapp\
- -Dversion=1.0-SNAPSHOT
+ -Dversion=0.0.1-SNAPSHOT
 
 pushd temp-sample-wicket-webapp
 mvn verify
